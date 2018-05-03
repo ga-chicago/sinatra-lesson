15067425e8dac7ad2b45a82581b5a1c5a8be3031
Reuben Ayres <ra@sent.com>
2018-05-03 14:04:39 -0500

    completed bonus portion of exercise

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,52 @@
 console.log("hey client js cool")
 
+let placeData = [];
+
 $('button').on('click', (event) => {
+  getInfo();
+  $('button').off('click')
+  $('button').remove()
+})
 
-
+function getInfo() {
   $.ajax({
     url: 'http://localhost:9292/places_to_go',
-    dataType: 'JSON', // if you forget this you would have to JSON.parse(the response)
+    dataType: 'JSON', 
     method: 'GET',
-    // success: function(response) {
-    //   console.log(response)
-    // }, 
-    success: makeDivs,
-    fail: function(error) {
+    success: function(response) {
+      placeData = response
+      showButtons();
+      makeDivs();
+    }, 
+    error: function(error) {
       console.error(error)
     }
   })
+}
 
-})
+function showButtons() {
+  placeData.forEach((place, i) => {
+    const $button = $('<button>').text(place.name)
+    $button.data('which', i)
+    $('#buttons').append($button);
+  })
 
-function makeDivs(response) {
-  response.forEach(place => {
+  $('#buttons').on('click', 'button', (e) => {
+    // console.log($(e.currentTarget).data('which'))
+    $('#places div').hide();
+    $('#place-'+ $(e.currentTarget).data('which').toString()).show();
+  })
+
+}
+
+function makeDivs() {
+  placeData.forEach((place, i) => {
     const $placeDiv = $('<div>').css({
       "background-image": "url(" + place.image + ")",
       "background-size": "cover",
-      "background-position": "center" /*,
-      "background-repeat": "no-repeat"*/
-    }).text(place.name + " (" + place.country + ") ")
+      "background-position": "center",
+      "display": "none"
+    }).text(place.name + " (" + place.country + ") ").attr("id", "place-" + i)
     $('#places').append($placeDiv)
   })
 }
