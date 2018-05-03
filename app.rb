d42ffa5db78c6ed35181610be6e53be7459cdcbd
Reuben Ayres <ra@sent.com>
2018-05-03 11:03:23 -0500

    a route that has params

--- app.rb
+++ app.rb
@@ -5,5 +5,31 @@ require 'bundler'
 Bundler.require()
 
 get '/' do
+
+  # this is like res.send(...) in express
+  # remember the last line of any method is what it will return
   "You just made a server! Good job u little coder u !! 😘"
+
 end
+
+# define a route -- do not have a trailing slash
+get '/hello' do
+  "Hello World"
+end
+
+
+#  URL parameters like so
+get '/hello/:some_param' do
+  # note: URL parameters are stored in params hash
+  # automatically for u
+  the_param = params['some_param']
+
+  "Hello, #{the_param}"
+
+end
+
+
+# render a template
+
+
+
